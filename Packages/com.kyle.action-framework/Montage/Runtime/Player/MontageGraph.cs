5c60ea306cb4708de16a41bc25c2b1c42850b2f9
张丹 <363130890@qq.com>
2026-02-01 15:14:27 +0800

    Update MontageGraph.cs

--- Packages/com.kyle.action-framework/Montage/Runtime/Player/MontageGraph.cs
+++ Packages/com.kyle.action-framework/Montage/Runtime/Player/MontageGraph.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.Playables;
@@ -8,9 +9,75 @@ namespace Montage
     [System.Serializable]
     public class MontageGraph
     {
+        private struct ConnectInfo
+        {
+            public PlayableHandle Handler;
+            public float Weight;
+        }
+
         public PlayableGraph Graph;
         public Animator Target;
+        private AnimationPlayableOutput output;
+        private AnimationMixerPlayable rootMixer;
+        private PlayableHandle[] connectInfos = new PlayableHandle[2];
         private readonly Stack<AnimationMixerPlayable> mixerPlayables = new Stack<AnimationMixerPlayable>();//回收池
+
+        public static MontageGraph Create(string name, Animator target)
+        {
+            var montageGraph = new MontageGraph();
+            montageGraph.Graph = PlayableGraph.Create(name);
+            montageGraph.Target = target;
+            montageGraph.output = AnimationPlayableOutput.Create(montageGraph.Graph, "MontageOutput", target);
+            montageGraph.rootMixer = AnimationMixerPlayable.Create(montageGraph.Graph, 2);
+            montageGraph.rootMixer.SetInputWeight(0, 0f);
+            montageGraph.rootMixer.SetInputWeight(1, 0f);
+            montageGraph.output.SetSourcePlayable(montageGraph.rootMixer);
+            return montageGraph;
+        }
+
+        public void ConnectToRoot<V>(V destination, float weight) where V : struct, IPlayable
+        {
+            for (int i = 0; i < connectInfos.Length; i++)
+            {
+                if(connectInfos[i] == destination.GetHandle())
+                {
+                    //已经连接过了，更新权重
+                    rootMixer.SetInputWeight(i, weight);
+                    var info = connectInfos[i];
+                    connectInfos[i] = info;
+                    return;
+                }
+                else if(connectInfos[i] == PlayableHandle.Null)
+                {
+                    //找到空位，复用
+                    destination.ConnectInput(i, rootMixer, 0);
+                    rootMixer.SetInputWeight(i, weight);
+                    connectInfos[i] = destination.GetHandle();
+                    return;
+                }
+            }
+            //没有空位，扩容
+            int nexIndex = connectInfos.Length;
+            Array.Resize(ref connectInfos, connectInfos.Length + 1);
+            rootMixer.SetInputCount(connectInfos.Length);
+            destination.ConnectInput(nexIndex, rootMixer, 0);
+            rootMixer.SetInputWeight(nexIndex, weight);
+            connectInfos[nexIndex] = destination.GetHandle();
+        }
+
+        public void DisConnectFromRoot<V>(V playable) where V : struct, IPlayable
+        {
+            for (int i = 0; i < connectInfos.Length; i++)
+            {
+                if(connectInfos[i] == playable.GetHandle())
+                {
+                    rootMixer.DisconnectInput(i);
+                    rootMixer.SetInputWeight(i, 0f);
+                    connectInfos[i] = PlayableHandle.Null;
+                    return;
+                }
+            }
+        }
 
         public AnimationMixerPlayable GetMixerPlayable()
         {
